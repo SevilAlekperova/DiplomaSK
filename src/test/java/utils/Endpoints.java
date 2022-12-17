c0b5d5b967cdd3e9164cf1a3439a6d8a4d35323c
KatG17 <katsiaryna.hamarnik@gmail.com>
2022-12-17 09:34:06 +0300

    API Tests

---
+++ src/test/java/utils/Endpoints.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Endpoints {
+    public static final String GET_All_USERS = "/index.php?/api/v2/get_users";
+    public static final String GET_USER = "/index.php?/api/v2/get_user/{user_id}";
+    public static final String CREATE_PROJECT = "/index.php?/api/v2/add_project";
+    public static final String GET_PROJECT = "/index.php?/api/v2/get_project/{project_id}";
+    public static final String DELETE_PROJECT = "/index.php?/api/v2/delete_project/{project_id}";
+}
